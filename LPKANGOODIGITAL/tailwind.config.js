a5f2d86893ad780e5ae6c5c65c6d8484e80f5a28
Bugs.dat <89652212+MariaIsabelli@users.noreply.github.com>
2024-05-20 14:25:05 -0300

    Landing Page Kangoo Digital

---
+++ LPKANGOODIGITAL/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        blue:{
+          '50': '#00A1CC',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
